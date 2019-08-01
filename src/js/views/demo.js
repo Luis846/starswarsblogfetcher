2e7057211619fa4008480e38bb53286a608cd7b9
Luis Rivera <luis.rivera846@gmail.com>
2019-08-01 22:52:41 +0000

    added

--- src/js/views/demo.js
+++ src/js/views/demo.js
@@ -14,7 +14,9 @@ export class Demo extends React.Component {
 						{({ store }) => {
 							return (
 								<div>
-									<h1>Characters</h1>
+									<h1>
+										<u>Characters</u>
+									</h1>
 									{store.people.map((item, index) => {
 										return (
 											<li key={index} className="list-group-item d-flex justify-content-between">
@@ -24,7 +26,9 @@ export class Demo extends React.Component {
 									})}
 
 									<br />
-									<h1>Vehicles</h1>
+									<h1>
+										<u>Vehicles</u>
+									</h1>
 									{store.vehicles.map((item, index) => {
 										return (
 											<li key={index} className="list-group-item d-flex justify-content-between">
@@ -34,7 +38,9 @@ export class Demo extends React.Component {
 									})}
 
 									<br />
-									<h1>Planets</h1>
+									<h1>
+										<u>Planets</u>
+									</h1>
 									{store.planets.map((item, index) => {
 										return (
 											<li key={index} className="list-group-item d-flex justify-content-between">
